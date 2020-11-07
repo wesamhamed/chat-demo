35d8cc0142dd092c20182a0ac0aa893d0c29a0fe
wesam <eng,wesam.m.hamed@gmail.com>
2020-11-07 13:45:23 +0200

    first commit

---
+++ Desktop/nodejs/chatcord/public/js/main.js
@@ -0,0 +1,70 @@
+const chatForm = document.getElementById("chat-form");
+const chatMessages = document.querySelector(".chat-messages");
+const roomName = document.getElementById('room-name');
+const userList = document.getElementById('users');
+//Get username and room from URL
+const { username, room } = Qs.parse(location.search, {
+    ignoreQueryPrefix: true
+});
+
+const socket = io();
+
+//Join chatroom
+socket.emit("joinRoom", { username, room });
+
+//Get room and users
+socket.on("roomUsers", ({ room, users }) => {
+    outputRoomName(room);
+    outputUsers(users);
+});
+//Message from server
+socket.on("message", message => {
+    outputMessage(message);
+
+    //Scroll down
+    chatMessages.scrollTop = chatMessages.scrollHeight;
+});
+
+//Message submit
+chatForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const msg = event.target.elements.msg.value;
+    //Emit message to server
+    socket.emit("chatMessage", msg);
+
+    //Clear input
+    event.target.elements.msg.value = '';
+    event.target.elements.msg.focus();
+});
+
+//Output message to DOM
+
+// Output message to DOM
+function outputMessage(message) {
+    const div = document.createElement('div');
+    div.classList.add('message');
+    const p = document.createElement('p');
+    p.classList.add('meta');
+    p.innerText = message.username;
+    p.innerHTML += `<span>${message.time}</span>`;
+    div.appendChild(p);
+    const para = document.createElement('p');
+    para.classList.add('text');
+    para.innerText = message.text;
+    div.appendChild(para);
+    document.querySelector('.chat-messages').appendChild(div);
+}
+
+// Add room name to DOM
+function outputRoomName(room) {
+    roomName.innerText = room;
+}
+// Add users to DOM
+function outputUsers(users) {
+    userList.innerHTML = '';
+    users.forEach(user => {
+        const li = document.createElement('li');
+        li.innerText = user.username;
+        userList.appendChild(li);
+    });
+}
